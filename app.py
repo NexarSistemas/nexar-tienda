3ee6ed18adf84ef4df06c26ea8b0a46f99483e40
rolojnb <mailxsuscripcion@gmail.com>
2026-04-14 13:16:00 -0300

    Se quita consola de app y se quita los acceso anteirores de admin y usuarios predefinidos

--- app.py
+++ app.py
@@ -2525,4 +2525,4 @@ if __name__ == '__main__':
     print(f"║    Configuración inicial requerida al iniciar  ║")
     print(f"╚═══════════════════════════════════════════════╝")
 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
